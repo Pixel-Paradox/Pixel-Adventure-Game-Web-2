5d608eba12e11423123d7b2e8878a795b3e02cd7
Vital Vuillaume <vitalvuillaume@proton.me>
2023-10-14 20:03:37 +0200

    Add files via upload

--- Pixel-Adventure-Game-Web.js
+++ Pixel-Adventure-Game-Web.js
@@ -4,6 +4,7 @@ const audio = document.querySelector(".audio");
 const menu = document.querySelector(".menu");
 const reprendre = document.querySelector(".reprendre");
 const ecran = document.querySelector(".ecran");
+const musique = document.querySelector(".musique");
 const body = document.body;
 
 let posX = 0;
@@ -54,17 +55,66 @@ document.addEventListener('keydown', function(event) {
 
         window.scrollTo(diffX, diffY);
     }
-    //audio.play();
+    audio.play();
 });
 
 document.addEventListener("keydown", function(event) {
     if (event.key === "Escape") {
         menu.classList.toggle("active");
+        body.classList.toggle("active");
     }
 });
 
 reprendre.onclick = function() {
     menu.classList.remove("active");
+};
+
+let pleinEcranActif = false;
+
+ecran.onclick = function() {
+    if (!pleinEcranActif) {
+        enterFullscreen();
+        pleinEcranActif = true;
+        ecran.textContent = "Enlever le pleine écran";
+    } else {
+        exitFullscreen();
+        pleinEcranActif = false;
+        ecran.textContent = "Mettre en pleine écran";
+    }
+};
+
+function enterFullscreen() {
+    if (document.documentElement.requestFullscreen) {
+        document.documentElement.requestFullscreen();
+    } else if (document.documentElement.mozRequestFullScreen) {
+        document.documentElement.mozRequestFullScreen();
+    } else if (document.documentElement.webkitRequestFullscreen) {
+        document.documentElement.webkitRequestFullscreen();
+    } else if (document.documentElement.msRequestFullscreen) {
+        document.documentElement.msRequestFullscreen();
+    }
+}
+
+function exitFullscreen() {
+    if (document.exitFullscreen) {
+        document.exitFullscreen();
+    } else if (document.mozCancelFullScreen) {
+        document.mozCancelFullScreen();
+    } else if (document.webkitExitFullscreen) {
+        document.webkitExitFullscreen();
+    } else if (document.msExitFullscreen) {
+        document.msExitFullscreen();
+    }
+}
+
+musique.onclick = function() {
+    if (audio.paused) {
+        audio.play();
+        musique.textContent = "Enlever la musique";
+    } else {
+        audio.pause();
+        musique.textContent = "Mettre la musique";
+    }
 };
 
 if (localStorage.getItem('popupClosed') === 'true') {
